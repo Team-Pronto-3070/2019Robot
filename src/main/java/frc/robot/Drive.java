4b46b516c8563e5ce27bb5b723938cba09fce844
Kyle McRae <kyle.mcrae1@gmail.com>
2019-01-24 15:18:20 -0800

    added ctre libraries
    it drives now

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -1,42 +1,61 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import com.ctre.phoenix.motorcontrol.can.*;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.*;
+
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.Encoder;
 
 
 public class Drive implements Pronstants {
 
-    Talon talonFL, talonBL, talonFR, talonBR, talon1, talon2;
+    TalonSRX talonFL, talonBL, talonFR, talonBR, talon1, talon2;
     Joystick joyL, joyR;
-
+    ADIS16448_IMU imu;
+    Encoder encL, encR;
         
 
     public Drive(ADIS16448_IMU imu)  {
         
-        talon1 = new TalonSRX(TALON1_PORT);
-        talon2 = new TalonSRX(TALON2_PORT); 
+        // talon1 = new TalonSRX(TALON1_PORT);
+        // talon2 = new TalonSRX(TALON2_PORT); 
 
         talonFL = new TalonSRX(TALONFL_PORT);
         talonBL = new TalonSRX(TALONBL_PORT);
         talonFR = new TalonSRX(TALONFR_PORT);
         talonBR = new TalonSRX(TALONBR_PORT);
 
+        talonFL.setInverted(true);
+        talonBL.setInverted(true);
+        talonFR.setInverted(false);
+        talonBR.setInverted(false);
+
+        talonFL.configFactoryDefault();
+        talonBL.configFactoryDefault();
+        talonFR.configFactoryDefault();
+        talonBR.configFactoryDefault();
+
+        talonFL.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+        talonFR.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+
+
         joyL = new Joystick(JOYL_PORT);
         joyR = new Joystick(JOYR_PORT);
-
-        imu= new ADIS16448_IMU();
+        this.imu = imu;
 
     }
 
 
     public void leftDrive(double power) {
 
-    talonFL.set(power);
+    talonFL.set(ControlMode.PercentOutput, power);
     talonBL.set(ControlMode.Follower, TALONFL_PORT);
     }
 
     public void rightDrive(double power){
-    talonFR.set(power);
+    talonFR.set(ControlMode.PercentOutput, power);
     talonBR.set(ControlMode.Follower, TALONFR_PORT);
     }
     
@@ -45,30 +64,35 @@ public class Drive implements Pronstants {
         leftDrive(0);
     }
 
-    public void tankDrive(double power) {
+    public void tankDrive(double joyL, double joyR) {
 
-        if(Math.abs(joyL.getRawAxis(1)) > DEADZONE){
-            leftDrive(joyL.getRawAxis(power));
+        if(Math.abs(joyL) > DEADZONE){
+            leftDrive(joyL/3.0);
+        }else{
+            leftDrive(0);
         }
-        if(Math.abs(joyR.getRawAxis(1)) > DEADZONE){
-            rightDrive(joyR.getRawAxis(power));
+        if(Math.abs(joyR) > DEADZONE){
+            rightDrive(joyR/3.0);
+        }else{
+            rightDrive(0);
         }
     }
 
     public void driveToAngle(double angle) {
-        imu.reset();
-        if((imu.getAngle() - angle) >= GYRO_DEADZONE){
+        if((imu.getYaw() - angle) >= GYRO_DEADZONE){
                 rightDrive(-TURN_SPEED);
                 leftDrive(TURN_SPEED);
             }          
-        }
-        else if(gyro.getAngle() - angle < GYRO_DEADZONE){
+        
+        else if((imu.getYaw() - angle) < GYRO_DEADZONE){
             rightDrive(TURN_SPEED);
             leftDrive(-TURN_SPEED);
         }
         else{
             stop();
         }
+
+
     }
 
 }
