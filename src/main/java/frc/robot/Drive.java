0a6677185ed64ac8adec21f12e2f75858a487631
beechnutkjr <xseggs@gmail.com>
2019-01-26 09:05:38 -0800

    Commented pronstants

--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -4,8 +4,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.*;
-
-import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Encoder;
 
 
--- src/main/java/frc/robot/Pronstants.java
+++ src/main/java/frc/robot/Pronstants.java
@@ -1,21 +1,21 @@
 package frc.robot;
 
 public interface Pronstants {
-
-    public static final int TALON1_PORT = 1;
+    
+    public static final int TALON1_PORT = 1; //Talon ports
     public static final int TALON2_PORT = 2;
     public static final int TALONFL_PORT = 15;
     public static final int TALONBL_PORT = 14;
     public static final int TALONFR_PORT = 12;
     public static final int TALONBR_PORT = 13;
 
-    public static final int JOYL_PORT = 0;
+    public static final int JOYL_PORT = 0; //Joystick ports
     public static final int JOYR_PORT = 1;
 
-    public static final double DEADZONE = .2;
-    public static final double GYRO_DEADZONE = 4.6;
+    public static final double DEADZONE = .2; //Joystick deadzone
+    public static final double GYRO_DEADZONE = 4.6; //Gyro deadzone
 
-    public static final double TURN_SPEED = .5;
+    public static final double TURN_SPEED = .5; //Bot turning speed
 
-    public static final double TAL_MAX_VALUE = 400;
+    public static final double TAL_MAX_VALUE = 400; //Talon maximum speed in RPM
 }
