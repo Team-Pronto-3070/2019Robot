0da7d2ff6590c4afb13ac5d924599b1b2a93588a
Kyle McRae <kyle.mcrae1@gmail.com>
2019-02-02 10:55:07 -0800

    added encoder on the arm to the smart dashboard
    also added  comments to the smart dashboard stuff

--- src/main/java/frc/robot/Pronstants.java
+++ src/main/java/frc/robot/Pronstants.java
@@ -3,17 +3,17 @@ package frc.robot;
 public interface Pronstants {
     
     public static final int ARMTAL1_PORT = 1; //Talon ports
-    public static final int ARMTAL2_PORT = 2;
+    public static final int ARMTAL2_PORT = 2; //the arm ports need adjustment
     public static final int TALONFL_PORT = 15;
     public static final int TALONBL_PORT = 14;
-    public static final int TALONFR_PORT = 12;
-    public static final int TALONBR_PORT = 13;
+    public static final int TALONFR_PORT = 12;//15
+    public static final int TALONBR_PORT = 13;//14
 
     public static final int JOYL_PORT = 0; //Joystick ports
     public static final int JOYR_PORT = 1;
     public static final int ARMCONT_PORT = 2;
 
-   
+
 
 
     public static final double DEADZONE = .2; //Drive joystick deadzone
